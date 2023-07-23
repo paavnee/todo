3a5986d4647218bb8a3bacab58d4ac8207510525
pavni <pavni2406@gmail.com>
2023-07-23 22:42:11 +0530

    add files

--- src/firebase.js
+++ src/firebase.js
@@ -7,15 +7,6 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAY6Duat-lA9vLmUdPq2F3RY-qdRvTx7ak",
-  authDomain: "todo-app-21ee6.firebaseapp.com",
-  projectId: "todo-app-21ee6",
-  storageBucket: "todo-app-21ee6.appspot.com",
-  messagingSenderId: "3467900389",
-  appId: "1:3467900389:web:0068a8b05a1756c8d2ab0c",
-  measurementId: "G-NZ7BXW3DSR",
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
